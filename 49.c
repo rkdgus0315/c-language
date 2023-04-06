35eaf72a962eeb95c8ab54dd002e311a47ad61d8
OSXऌॢ꼬꼬 G <rkdgus1234567899@gmail.com>
2023-04-06 05:19:35 +0000

    49.c

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int a = 16;
+	float b = 3.14;
+	char munja = 'm';
+	char munjas[5] = "ansan";
+	printf("%d\n",num1);
+	printf("%f\n",num2);
+	printf("%c\n",munja);
+	printf("%s\n",)
+	return 0;
+}
