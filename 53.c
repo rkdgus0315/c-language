54ad2ea3e9ddecb7ebf8e8a444c14f1d197fd3df
OSXऌॢ꼬꼬 G <rkdgus1234567899@gmail.com>
2023-04-06 05:55:10 +0000

    53.c

---
+++ 53.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() {
+	
+	bool a = false;
+	bool b = 25;
+	printf("%d $d\n", a, 0);
+	peintf("$d %d\n", b, true);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+}
+
+
+
