512d46e5269d09b3d774d58bb8eaf245f79f10be
OSXऌॢ꼬꼬 G <rkdgus1234567899@gmail.com>
2023-04-06 05:19:29 +0000

    47b.c

---
+++ 47b.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+
+    int main()
+{
+	    int num1=16;
+		int num2;
+		int sum=0; int sub=0;
+		num2 = 61;
+		sum = num1 + num2;
+		sub = num1 - num2;
+		printf("%d + %d = %d\n",num1 ,num2 ,sum);
+		printf("%d - %d = %d\n",num1 ,num2 ,sub);
+		return 0;
+}
