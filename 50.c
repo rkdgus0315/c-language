5d20e8de9525e8ee3e5bdd65f82625137a62bdc9
OSXऌॢ꼬꼬 G <rkdgus1234567899@gmail.com>
2023-04-06 05:19:42 +0000

    50.c

---
+++ 50.c
@@ -0,0 +1,26 @@
+ #include <stdio.h>
+
+int main() {
+	unsigned int max = 2147483647;
+	printf("%u\n", max);
+	max = max + 1;
+	printf("%uu\n", max);
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
