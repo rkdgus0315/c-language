e349809d6751c2fa274dcfae3d9850b78a61dc0b
OSXऌॢ꼬꼬 G <rkdgus1234567899@gmail.com>
2023-04-06 05:19:54 +0000

    51.c

---
+++ 51.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+int main () {
+	char a ='a';
+	char b = a + 1; //66
+	printf("ascII[%d] = %c \n", a,a);
+	printf("ascII[%d] = %c \n", b, b);
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+}
